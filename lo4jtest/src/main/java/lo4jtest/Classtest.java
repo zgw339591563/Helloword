105c01d547bd9513af48fad744494663c9448bab
33959156 <339591563@qq.com>
2017-08-16 11:46:46 +0800

    分支1

---
+++ lo4jtest/src/main/java/lo4jtest/Classtest.java
@@ -0,0 +1,17 @@
+package lo4jtest;
+
+import org.apache.log4j.Logger;
+
+import java.util.logging.Level;
+
+public class Classtest {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(Classtest.class);
+
+	 public static void main(String[] args) {
+		int i=0;
+		
+	}
+}
