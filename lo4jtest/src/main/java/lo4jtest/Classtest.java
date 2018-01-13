db0fb8cac6577c06dec7f9b90655a990cc366485
zgw339591563 <339591563@qq.com>
2018-01-14 01:22:37 +0800

    主干删除logger

--- lo4jtest/src/main/java/lo4jtest/Classtest.java
+++ lo4jtest/src/main/java/lo4jtest/Classtest.java
@@ -1,6 +1,6 @@
 package lo4jtest;
 
-import org.apache.log4j.Logger;
+
 
 import java.util.logging.Level;
 
@@ -8,8 +8,6 @@ public class Classtest {
 	/**
 	 * Logger for this class
 	 */
-	private static final Logger logger = Logger.getLogger(Classtest.class);
-
 	 public static void main(String[] args) {
 		int i=0;
 
