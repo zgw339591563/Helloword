69cdb457abc2cefe9fb4bb2426b2b302a06260be
zgw339591563 <339591563@qq.com>
2018-01-14 01:26:25 +0800

    看是否会删除

--- lo4jtest/src/main/java/lo4jtest/Classtest.java
+++ lo4jtest/src/main/java/lo4jtest/Classtest.java
@@ -1,14 +1,12 @@
 package lo4jtest;
 
-import org.apache.log4j.Logger;
-
 import java.util.logging.Level;
 
 public class Classtest {
 	/**
 	 * Logger for this class
+	 * ssssssssssssssssssssssssssss
 	 */
-	private static final Logger logger = Logger.getLogger(Classtest.class);
 
 	 public static void main(String[] args) {
 		int i=0;
